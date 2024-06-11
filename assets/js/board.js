a441070d4cc5ad8ddcca5fc0fa060047855e350f
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-06-11 16:28:39 +0200

    setup popup for new task

---
+++ assets/js/board.js
@@ -0,0 +1,6 @@
+  // Open Popup
+  async function openDialog() {
+    document.getElementById("dialog").classList.remove("d-none");
+    document.body.classList.add("noscroll");
+    document.getElementById("innerDialog").classList.remove("d-none"); 
+  }

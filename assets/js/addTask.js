c46a8a210902b59fff1fa89935e34f3851b42997
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-06-11 12:11:20 +0200

    addTask/checkbox dropDown

---
+++ assets/js/addTask.js
@@ -0,0 +1,5 @@
+function showCheckboxes() {
+	let assignedTask = document.getElementById("assigned_task");
+	assignedTask.classList.toggle("assigned_task_hidden");
+	assignedTask.classList.toggle("assigned_task_show");
+}

b8afb54811581b74f48da4f90f71a0661ef60c1c
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-06-11 12:20:53 +0200

    addTask/function comments checkbox

--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -1,5 +1,9 @@
+/**
+ * This Function is used to show and hidde thechekBoxes in the AddTask html.
+ * The Field name is assigned to
+ */
 function showCheckboxes() {
-	let assignedTask = document.getElementById("assigned_task");
-	assignedTask.classList.toggle("assigned_task_hidden");
-	assignedTask.classList.toggle("assigned_task_show");
+	let assignedTask = document.getElementById("assigned-task");
+	assignedTask.classList.toggle("assigned-task-hidden");
+	assignedTask.classList.toggle("assigned-task-show");
 }

ec3ad23697e40b5d02a38937e68349f9d3156616
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-06-11 17:04:12 +0200

    popup add task in board close

--- assets/js/board.js
+++ assets/js/board.js
@@ -4,3 +4,9 @@
     document.body.classList.add("noscroll");
     document.getElementById("innerDialog").classList.remove("d-none"); 
   }
+
+  function closeDialog() {
+    document.getElementById("dialog").classList.add("d-none");
+    document.getElementById("innerDialog").classList.add("d-none");
+    document.body.classList.remove("noscroll");
+  }

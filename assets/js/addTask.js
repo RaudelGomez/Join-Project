c8de646c7b09cd7cd554128ab39b6cae91b7cbbe
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-06-12 14:36:57 +0200

    addTask/Dropdown category done

--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -18,3 +18,26 @@ function changePriority(idPriorityButton) {
 	document.getElementById('button-low-priority').classList.remove('active')
 	document.getElementById(`${idPriorityButton}`).classList.add('active')
 }
+
+/**
+ * This Function is used to show and hidde the options in the dropdown category in addTask page
+ * Field name Category
+ */
+function showTaskOption() {
+	let categoryTask = document.getElementById("category-task");
+	categoryTask.classList.toggle("assigned-task-hidden");
+	categoryTask.classList.toggle("assigned-task-show");
+	document.getElementById('option-selected').textContent = "Select task category";
+}
+
+/**
+ * This Function is used to select the category from the options in the dropdown category in addTask page
+ * Field name Category
+ */
+function categorySelected(idTask) {
+	showTaskOption();
+	let optionSelected = document.getElementById('option-selected');
+	let taskValue = document.getElementById(`${idTask}`).textContent;
+	console.log(taskValue);
+	optionSelected.textContent = taskValue;
+}

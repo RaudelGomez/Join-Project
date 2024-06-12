2c7be793390f4d6e6a69d6a46a6de7daad826172
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-06-12 17:56:07 +0200

    task popup checkbox style

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,5 +1,6 @@
   // Open Popup
   async function openDialog(template) {
+    // console.log(template);
     // const htmlTemplate = 
     // document.getElementById("template").setAttribute('w3-include-html','./assets/templates/'+template);
     document.getElementById("innerDialog").classList.remove("d-none"); 

a780bed4b76af3f034e6cd6a8aeb64e4b99f51cd
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-06-12 08:19:58 +0200

    board popup animation with close icon

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,12 +1,20 @@
   // Open Popup
   async function openDialog() {
-    document.getElementById("dialog").classList.remove("d-none");
-    document.body.classList.add("noscroll");
     document.getElementById("innerDialog").classList.remove("d-none"); 
+    document.getElementById("innerDialog").classList.remove("animate__slideOutRight");
+    document.getElementById("innerDialog").classList.add("animate__slideInRight");
+    document.getElementById("dialog").classList.add("animate__fadeIn");
+    document.getElementById("dialog").classList.remove("animate__fadeOut");
+    document.getElementById("dialog").classList.remove("d-none");
+    document.body.classList.add("noscroll");    
   }
 
   function closeDialog() {
-    document.getElementById("dialog").classList.add("d-none");
-    document.getElementById("innerDialog").classList.add("d-none");
+    document.getElementById("dialog").classList.remove("animate__fadeIn");
+    document.getElementById("dialog").classList.add("animate__fadeOut");
+    document.getElementById("innerDialog").classList.add("animate__slideOutRight");
+    document.getElementById("innerDialog").classList.remove("animate__slideInRight");
+    setTimeout(() => {document.getElementById("dialog").classList.add("d-none")}, 500);
     document.body.classList.remove("noscroll");
   }
+

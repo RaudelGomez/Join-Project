66380f802f0f249b11f3d8bb8bedb4502278c821
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-06-13 10:20:47 +0200

    signUp/registerFunction

--- assets/js/signUp.js
+++ assets/js/signUp.js
@@ -1,13 +1,10 @@
-let contacts = [];
-
 function registUser() {
     let nameIdSignUp = document.getElementById("nameIdSignUp");
     let emailIdSignUp = document.getElementById("emailIdSignUp");
     let passwordIdSignUp = document.getElementById("passwordIdSignUp");
     let passwordConfirmIdSignUp = document.getElementById("passwordConfirmIdSignUp");
     let confirmSignUp = document.getElementById("confirmSignUp");
-    console.log(confirmSignUp);
-
+  
     let contact = {
         "name": nameIdSignUp.value,
         "email": emailIdSignUp.value,
@@ -15,17 +12,14 @@ function registUser() {
         "user": true,
         "id":"",
         "password": passwordIdSignUp.value,
-
     }
     if (passwordIdSignUp.value === passwordConfirmIdSignUp.value && confirmSignUp.checked == true) {
-        
         contacts.push(contact);
         nameIdSignUp.value = "";
         emailIdSignUp.value = "";
         passwordIdSignUp.value = "";
         passwordConfirmIdSignUp.value = "";
         confirmSignUp.checked = false;
-
     } else {
         alert("Password incorrect!")
     }

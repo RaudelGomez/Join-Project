3d3e68d72648948b000038ea3722efa362986935
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-06-19 15:54:20 +0200

    Summray/Task date

--- assets/js/summary.js
+++ assets/js/summary.js
@@ -1,5 +1,11 @@
-getTimeOfDay();
-loadLocalStorage();
+async function initSummary(){
+  await loadData('tasks');
+  getTimeOfDay();
+  loadLocalStorage();
+  updatingSummaryData();
+}
+
+initSummary();
 
 /**
  * This function greets with the moment of the day. Ex: Good Night
@@ -30,5 +36,93 @@ function loadLocalStorage() {
     userName = JSON.parse(sessionStorage.getItem("Join")).userName;
   }
   document.getElementById('person-logged-name').textContent = userName;
+}
+
+/**
+ * This function update the summary task
+ */
+function updatingSummaryData(){
+  let tasksArray = Object.values(tasks);
+  let tasksCount = tasksArray.length;
+  let toDoCount = countTaskSummary(tasksArray, 1);
+  let progressCount = countTaskSummary(tasksArray, 2);
+  let awaitCount = countTaskSummary(tasksArray, 3);
+  let doneCount = countTaskSummary(tasksArray, 4);
+  let urgentTaskCount = countTaskUrgent(tasksArray);
+  let closerUrgentDate = calculateDatePrio();
+  document.getElementById('quantity-task').textContent = `${toDoCount}`;
+  document.getElementById('quantity-done').textContent = `${doneCount}`;
+  document.getElementById('progress-in-task').textContent = `${progressCount}`;
+  document.getElementById('progress-awaiting-feedback').textContent = `${awaitCount}`;
+  document.getElementById('task-in-board').textContent = `${tasksCount}`;
+  document.getElementById('quantity-urgent').textContent = `${urgentTaskCount}`;
+  document.getElementById('date-urgent').textContent = `${closerUrgentDate}`;
+}
+
+/**
+ * This function count different value, dependes of the parameters.
+ * @param {Array} tasksArray - The array tasks
+ * @param {number} state This the number of the status of the Task
+ * @returns 
+ */
+function countTaskSummary(tasksArray, state){
+  let allTasks = tasksArray.filter(filterTasks);
+  let taskLength = allTasks.length;
+  return taskLength;
+
+  function filterTasks(task){
+    return task.status == state;
+  }
+}
+
+/**
+ * This function count how many urgent tasks are
+ * @param {Array} tasksArray - That is the array tasks
+ * @returns - count of tasks with the priotity: urgent
+ */
+function countTaskUrgent(tasksArray){
+  let allTasks = tasksArray.filter(tasksUrgent);
+  let urgentLength = allTasks.length;
+  return urgentLength;
+
+  function tasksUrgent(task){
+    return task.priorityTask == "urgent";
+  }
+}
+
+/**
+ * This function return the closer urgent date of the task
+ * @returns The closer Date
+ */
+function calculateDatePrio() {
+  let allTask = Object.values(tasks);
+  let datesArray = allTask.map(date => date.timeDeadlineTask);
+  const today = new Date();
+  // Initialize variables for the nearest date and the minimum difference
+  let dateCloser = null;
+  let minimunDiferenceDate = Infinity;
+
+  datesArray.map(date => {
+    const currentDate = new Date(date);
+    const difference = diferenciaEnDias(currentDate, today);
+    // Update if we find a date with a smaller difference
+    if (difference < minimunDiferenceDate) {
+      minimunDiferenceDate = difference;
+      dateCloser = date;
+    }
+  });
+  console.log(dateCloser);
+  return dateCloser;
+}
+
+/**
+ * Function to calculate the difference in days between two dates
+ * @param {Date} date1 - First Date to compare
+ * @param {Date} date2 - First Date to compare
+ * @returns 
+ */
+function diferenciaEnDias(date1, date2) {
+  const oneDay = 24 * 60 * 60 * 1000; // Milliseconds in a day
+  return Math.abs((date1 - date2) / oneDay);
 }
 

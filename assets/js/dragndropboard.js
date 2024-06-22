6691e12cdf20d3eae8f83cff2ac81102b964a93f
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-06-22 09:04:46 +0200

    board no tasks containerhighlight drag-area and responsive work

--- assets/js/dragndropboard.js
+++ assets/js/dragndropboard.js
@@ -1,56 +1,8 @@
-///Save
-// let tasks = [{
-//     'id': 0,
-//     'title': 'Putzen',
-//     'description':'Beschreibung',
-//     'priority': 'urgent',
-//     'status':'inProgress',
-//     'category': 'User Story',
-//     'dueDate': 'date',
-//     'assignedTo' : 'user',
-//     'subtasks': {
-// 'subtask': 'unteraufgabe'
-//     }
-// }, {
-//     'id': 1,
-//     'title': 'Kochen',
-//     'category': 'inProgress'
-// }, {
-//     'id': 2,
-//     'title': 'Einkaufen',
-//     'category': 'toDo'
-// }];
-
-
-
-// function updateHTML() {
-//     let open = tasks.filter(t => t['status'] == 'open');
-
-//     document.getElementById('open').innerHTML = '';
-
-//     for (let index = 0; index < open.length; index++) {
-//         const element = open[index];
-//         document.getElementById('open').innerHTML += generateTodoHTML(element);
-//     }
-
-//     let closed = tasks.filter(t => t['status'] == 'closed');
-
-//     document.getElementById('closed').innerHTML = '';
-
-//     for (let index = 0; index < closed.length; index++) {
-//         const element = closed[index];
-//         document.getElementById('closed').innerHTML += generateTodoHTML(element);
-//     }
-// }
 
 function startDragging(id, cardElement) {
     currentDragElement = id;
     cardElement.classList.add("rotateCard");
 }
-
-// function generateTodoHTML(element) {
-//     return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">${element['title']}</div>`;
-// }
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -60,6 +12,10 @@ async function moveTo(status) {
     let firebaseURL=
     `/tasks/${currentDragElement}/status`; 
     await putData(status, firebaseURL);
+    document.getElementById("toDoBoard").classList.remove('drag-area-highlight');
+    document.getElementById("inProgress").classList.remove('drag-area-highlight');
+    document.getElementById("awaitFeedback").classList.remove('drag-area-highlight');
+    document.getElementById("done").classList.remove('drag-area-highlight');
     loadDataBoard();    
 }
 

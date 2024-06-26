1cbf948b280f319837c51395a4cf83f38c97f4de
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-06-26 09:18:48 +0200

    clean up contact.js

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,6 +1,6 @@
 let contactDetails = {};
 renderContacts();
-loadData("tasks");
+// loadData("tasks");
 
 /**
  * This function add a new contact in the contact page
@@ -49,13 +49,7 @@ function clearForm(formid) {
 function openNewContactPopup() {
   document.getElementById("template").classList.add("addContact");
   document.getElementById("template").classList.remove("editContact");
-  document.getElementById("innerDialog").classList.remove("d-none");
-  document.getElementById("innerDialog").classList.remove("animate__slideOutRight");
-  document.getElementById("innerDialog").classList.add("animate__slideInRight");
-  document.getElementById("dialog").classList.add("animate__fadeIn");
-  document.getElementById("dialog").classList.remove("animate__fadeOut");
-  document.getElementById("dialog").classList.remove("d-none");
-  document.body.classList.add("noscroll");
+  openPopup();
   document.getElementById("contactPopupHeadline").innerHTML = "Add contact";
   document.getElementById("contactPopupSubHeadline").classList.remove("d-none");
   document.getElementById("contactSaveButton").innerHTML = "Create contact";
@@ -69,12 +63,10 @@ function openNewContactPopup() {
   document.getElementById("profileIcon").innerHTML = /* HTML */ ` <img src="./assets/img/person_fill.svg" alt="" /> `;
 }
 
-async function openEditContactPopup(iconColor, contactName, contactMail, initials, results, phone, id) {
-  document.getElementById("template").classList.remove("addContact");
-  document.getElementById("template").classList.add("editContact");
-  document.getElementById("profileIcon").innerHTML = /* HTML */ `
-    <span id="popupInitials" class="profileIconBig" style="background-color: ${iconColor};">${initials}</span>
-  `;
+/**
+ * Just open a animated popup
+ */
+function openPopup() {
   document.getElementById("innerDialog").classList.remove("d-none");
   document.getElementById("innerDialog").classList.remove("animate__slideOutRight");
   document.getElementById("innerDialog").classList.add("animate__slideInRight");
@@ -82,6 +74,25 @@ async function openEditContactPopup(iconColor, contactName, contactMail, initial
   document.getElementById("dialog").classList.remove("animate__fadeOut");
   document.getElementById("dialog").classList.remove("d-none");
   document.body.classList.add("noscroll");
+}
+
+/**
+ * Opens the popup for edit contact
+ * @param {string} iconColor - The hexcode of the profile icon color
+ * @param {string} contactName - the name of the contact
+ * @param {string} contactMail - the mail of the contact
+ * @param {string} initials - the initials of the contact
+ * @param {string} results - the firebase key
+ * @param {string} phone - the phone number of the contact
+ * @param {integer} id - the index number of the sorted array
+ */
+async function openEditContactPopup(iconColor, contactName, contactMail, initials, results, phone, id) {
+  document.getElementById("template").classList.remove("addContact");
+  document.getElementById("template").classList.add("editContact");
+  document.getElementById("profileIcon").innerHTML = /* HTML */ `
+    <span id="popupInitials" class="profileIconBig" style="background-color: ${iconColor};">${initials}</span>
+  `;
+  openPopup();
   document.getElementById("contactPopupHeadline").innerHTML = "Edit contact";
   document.getElementById("contactPopupSubHeadline").classList.add("d-none");
   document.getElementById("contactSaveButton").innerHTML = "Save";
@@ -98,6 +109,11 @@ async function openEditContactPopup(iconColor, contactName, contactMail, initial
   document.getElementById("phone").value = phone;
 }
 
+/**
+ * This funtion updates the contact details after editing in the firebase db as well as on the display
+ * @param {string} id - Firebase Key of the contact
+ * @param {integer} index - Index Number of the sorted contacts array (contactDetails)
+ */
 async function updateContact(id, index) {
   let name = document.getElementById("name").value;
   let mail = document.getElementById("email").value;
@@ -134,10 +150,16 @@ async function updateContact(id, index) {
   renderContacts();
 }
 
+/**
+ * Opens the small popup menu on mobile view with menu links for edit and delete contact
+ */
 function openContactButtons() {
   document.getElementById("contactMobileEditButtons").classList.remove("d-none");
 }
 
+/**
+ * Closes the small popup menu on mobile view with menu links for edit and delete contact
+ */
 function closeContactMenu() {
   document.getElementById("contactMobileEditButtons").classList.add("d-none");
   if (window.innerWidth > 1100) {
@@ -146,7 +168,7 @@ function closeContactMenu() {
 }
 
 /**
- * Render ale contacts with alphabetic order in the list
+ * Render all contacts with alphabetic order in the list
  */
 async function renderContacts() {
   let renderContacts = document.getElementById("contactListID");
@@ -188,6 +210,17 @@ async function renderContacts() {
   }
 }
 
+/**
+ * This function open and render the display of contact details
+ * @param {element} contactElement - The element in the contactlist which calls this function (active class will be added)
+ * @param {*} iconColor - the color of the profile icon
+ * @param {*} contactName - the name of the contact
+ * @param {*} contactMail - mail of the contact
+ * @param {*} initials - initials of the contact
+ * @param {*} results - Firebase Key
+ * @param {*} phone - phone of the contact
+ * @param {*} id - Index Number ot the sorted contacts array (contactDetails)
+ */
 function showContact(contactElement, iconColor, contactName, contactMail, initials, results, phone, id) {
   document
     .getElementById("editButton")
@@ -224,23 +257,37 @@ function showContact(contactElement, iconColor, contactName, contactMail, initia
   document.getElementById("deleteIDMobile").setAttribute("onclick", `deleteContact('${results}','${contactMail}') `);
 }
 
+/**
+ * Close the Display of Contact details
+ */
 function closeShowContact() {
   document.getElementById("contactDisplay").style.display = "none";
   closeContactMenu();
 }
 
+/**
+ * This function delete the contact and the assignment of tasks
+ * @param {string} firebaseKey - Contains the FirebaseKey
+ * @param {string} contactMail - Contains the Mailadress of the contact
+ */
 async function deleteContact(firebaseKey, contactMail) {
   await deleteData("/contacts/" + firebaseKey);
   const resultUrls = await findTaskUrlsByEmail(tasks, contactMail);
   deleteUserInTask(resultUrls);
   await loadData("tasks");
   renderContacts();
+  location.href = "./contacts.html";
 }
 
+/**
+ * Find all Tasks where the contact is assigned to and create Array with the urls
+ * @param {array} tasks - Contains all tasks
+ * @param {string} email - The Email adress of the contact
+ * @returns
+ */
 async function findTaskUrlsByEmail(tasks, email) {
   const urls = [];
-  let key;
-  let i;
+  let key, i;  
   for (key in tasks) {
     if (tasks.hasOwnProperty(key)) {
       const task = tasks[key];
@@ -259,6 +306,10 @@ async function findTaskUrlsByEmail(tasks, email) {
   return urls;
 }
 
+/**
+ * This function delete the assignment of contact in tasks
+ * @param {array} urls - contains the urls with all tasks where the contact is assigned to
+ */
 async function deleteUserInTask(urls) {
   for (let i = 0; i < urls.length; i++) {
     const url = urls[i];
